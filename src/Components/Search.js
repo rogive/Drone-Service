7f8c2b10f8420c2fe033292f9c6263d2fa03ad50
Jonathan Palacio <jpalacio0612@gmail.com>
2020-08-14 18:13:13 -0500

    Se agrego la funcionalidad de departamentos

---
+++ src/Components/Search.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import newDepartments from '../data/deparments.json'
+
+
+class Checked extends React.Component{
+render(){
+    const { data } = this.props   
+    console.log(data)
+
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        {data.map((element)=>{
+          return(
+            <div> 
+              <div>
+                <input id={element.id} type="checkbox"/>
+                <label htmlFor={element.id}>{element.label}</label>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+
+
+class CitySearch extends React.Component{
+
+  state = {
+    deparmentID : null,
+    cityArray: []
+  }
+
+  handleChange = (event) => {
+    console.dir(event.target.id)
+    
+    this.setState({ deparmentID : event.target.id });
+
+  }
+
+  render(){
+      const { data } = this.props 
+      // const ciudades = ""
+      // const variable = data[this.state.deparmentID]  
+      
+  
+      return (
+        <div>
+
+            <div>
+              <h3>Departamentos</h3>
+              {data.map((element)=>{
+                return(
+                  <div> 
+                    <div>
+                      <input id={element.id} type="checkbox" onChange={this.handleChange}/>
+                      <label htmlFor={element.id}>{element.label}</label>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+            <div>
+            <h3>Ciudades</h3>
+            {this.state.deparmentID === null ? <h1>falso</h1> : <h1>Verdadero</h1>}
+          </div>
+        </div>
+        
+      )
+    }
+  }
+
+
+const categories = [
+  {
+      id: "fotos",
+      label: "Fotos"
+  },
+  {
+    id: "videos",
+    label: "Videos"
+  },
+  {
+    id: "fotogrametria",
+    label: "Fotogrametria"
+  },
+  {
+    id: "ortomapas",
+    label: "Ortomapas"
+  },
+  {
+    id: "modeloDigital",
+    label: "Modelo Digital"
+  }
+]
+
+// const stringifyDeparments = JSON.stringify(deparments)
+// const newDepartments = JSON.parse(stringifyDeparments)
+
+
+
+class Search extends React.Component{
+
+  render(){
+      return(
+        <div>
+          <input class="buscar" type="text" placeholder="Buscar"/>
+          <Checked name="Categorias" data={categories}/>
+          <CitySearch data={newDepartments}/>
+        </div>
+     )
+  }
+}
+
+export default Search
