fb35dbd64bc23d3438603be1f4743205b22fada0
Jonathan Palacio <jpalacio0612@gmail.com>
2020-08-19 10:15:38 -0500

    Resolucion de conflictos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 import DronePilot from './Pages/DronePilot'; 
 import Home from './Pages/Home';
-<<<<<<< HEAD
 import Header from './Components/Header';
 import Explora from './Pages/Explora';
-import DronePilot from './Pages/DronePilot';
 import ClientProfile from './Pages/ClientProfile';
-import Error404 from './Pages/Error404'
+import Error404 from './Pages/Error404';
 import { Link, Router, BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
@@ -24,17 +22,8 @@ function App() {
         </Switch> 
     </BrowserRouter>  
   );
-=======
-import ClientProfile from './Pages/ClientProfile'
-
-function App() {
-  return (
-    <div>
-      {/* <Home/> */}
-      <ClientProfile/>
-    </div>
-  )
->>>>>>> 32be49cd163bce367e0b3892be4a3341b00e4aa6
 }
+
+
 
 export default App;
