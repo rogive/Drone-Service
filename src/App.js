32fae59b1da9fe6468969715a38281a3962cdebd
Jonathan Palacio <jpalacio0612@gmail.com>
2020-08-18 21:11:21 -0500

    Agregado el react-router-dom y navegacion entre home y explora

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
-import Explora from './Pages/Explora'
+import Header from './Components/Header';
+import Explora from './Pages/Explora';
+import Error404 from './Pages/Error404'
+import { Link, Router, BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Explora/>
+    <BrowserRouter>
+      <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/explora" component={Explora} />
+          <Route exact path="*" component={Error404}/>
+        </Switch> 
+    </BrowserRouter>  
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,74 +1,84 @@
-import React from 'react';
-
-import logo from '../../src/Logo-Drone.png';
-import styled from 'styled-components';
+import React from "react";
+import { Link, Router, BrowserRouter, Switch, Route } from "react-router-dom";
+import logo from "../../src/Logo-Drone.png";
+import styled from "styled-components";
 
 const HeaderContainer = styled.div`
-width: 90%;
-margin: 1rem auto;
-display: flex;
-`
+  width: 90%;
+  margin: 1rem auto;
+  display: flex;
+`;
 
 const HeaderLogoContainer = styled.div`
-flex: 1 1 30%;
-  img{
+  flex: 1 1 30%;
+  img {
     width: 16rem;
   }
-`
+`;
 
 const Ulist = styled.ul`
-display: flex;
-flex: 1 1 40%;
-font-size: 1.5rem;
-justify-content: space-between;
-text-decoration: none;
-list-style: none;
-align-items: center;
-font-family: 'Montserrat';
-`
+  display: flex;
+  flex: 1 1 40%;
+  font-size: 1.5rem;
+  justify-content: space-between;
+  text-decoration: none;
+  list-style: none;
+  align-items: center;
+  font-family: "Montserrat";
+`;
 
 const List = styled.li`
-cursor: pointer;
-padding: .3rem 1rem;
-transition: all .3s ease-in-out;
-&:hover{
-  transform: scale(1.2) rotate(-8deg);
-  box-shadow: .6rem .5rem .5rem rgba(0,0,0, .2);
-}
-`
+  cursor: pointer;
+  padding: 0.3rem 1rem;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    transform: scale(1.2) rotate(-8deg);
+    box-shadow: 0.6rem 0.5rem 0.5rem rgba(0, 0, 0, 0.2);
+  }
+`;
 
 const Session = styled.li`
-display: flex;
-flex: 1 1 30%;
-font-size: 1.5rem;
-justify-content: flex-end;
-text-decoration: none;
-list-style: none;
-align-items: center;
-font-family: 'Montserrat';
+  display: flex;
+  flex: 1 1 30%;
+  font-size: 1.5rem;
+  justify-content: flex-end;
+  text-decoration: none;
+  list-style: none;
+  align-items: center;
+  font-family: "Montserrat";
 
-.signup{
-  margin-right: 3rem;
-  font-weight: 600;
-}
+  .signup {
+    margin-right: 3rem;
+    font-weight: 600;
+  }
 
-.login{
-  font-weight: 600;
-}
-`
+  .login {
+    font-weight: 600;
+  }
+`;
 
-function Header(){
-  return(
+const StyledLink = styled(Link)`
+  cursor: pointer;
+  padding: 0.3rem 1rem;
+  transition: all 0.3s ease-in-out;
+  text-decoration: none;
+  color: black;
+  &:hover {
+    transform: scale(1.2) rotate(-8deg);
+    box-shadow: 0.6rem 0.5rem 0.5rem rgba(0, 0, 0, 0.2);
+  }
+`;
+
+function Header() {
+  return (
     <HeaderContainer>
       <HeaderLogoContainer>
-        <img width="100px" src = {logo} alt="logo_drone_services"/>
-
+        <img width="100px" src={logo} alt="logo_drone_services" />
       </HeaderLogoContainer>
       <Ulist>
-        <List>Home</List>
+        <StyledLink to="/">Home</StyledLink>
         <List>Blog </List>
-
-        <List>Explora</List>
+        <StyledLink to="/explora">Explora</StyledLink>
         <List>About</List>
         <List>Ayuda</List>
       </Ulist>
@@ -76,10 +86,9 @@ function Header(){
       <Session>
         <List className="signup">Sign Up</List>
         <List className="login">Login</List>
-      </Session> 
-
+      </Session>
     </HeaderContainer>
-  )
+  );
 }
 
-export default Header
+export default Header;
