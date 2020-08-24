f55cbc4adaec756593ea708e5ff34f4dbb6d94a1
ivan <ierodriguezgo@gmail.com>
2020-08-24 09:35:10 -0500

    issue: cors http

--- src/Components/Portfolios.js
+++ src/Components/Portfolios.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import { storage } from '../firebase';
+import axios from 'axios';
 
 const IconContainer = styled.div`
   width: 100%;
   text-align:center;
   padding-top: 3rem;
   img{
-    width: 15rem;
-    height: 15rem;
+    width: 15vw;
+    height: 15vw;
     border-radius: 3rem;
     filter: grayscale(100%);
   }
@@ -41,8 +42,8 @@ const ImageContainer = styled.div`
   border-radius: 1rem;
   margin: 1rem;
   img{
-    width: 20vh;
-    height: 20vh;
+    width: 10vw;
+    height: 10vw;
     border-radius: 3rem;
   }
 `
@@ -64,13 +65,24 @@ class Portfolios extends React.Component{
   state = {
     portfolios: [],
     name: "",
+    pilotId: "5f431d3ebd64571f5e5d63b7",
     id: 0,
     selectedFile: null,
-    url: ''
+    url: '',
+    error: '',
+    post: ''
+  }
+
+  componentDidMount() {
+    axios({
+      url: `localhost:8000/media/listar/piloto/${this.state.pilotId}`,
+      method: 'GET',
+    })
+      .then(({ data }) => this.setState({ portfolios: data }, ()=>console.dir(data)))
+      .catch((error) => this.setState({ error }))
   }
 
   handleChange = (event) => {
-    console.dir(event.target)
     this.setState({ 
       name: event.target.files[0].name,
       selectedFile: event.target.files[0]
@@ -80,33 +92,41 @@ class Portfolios extends React.Component{
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const uploadImage = storage.ref('Pilots/Pilot-x/' + this.state.name).put(this.state.selectedFile);
+    const uploadImage = storage.ref(`Pilots/Pilot-${this.state.pilotId}/` + this.state.name).put(this.state.selectedFile);
 
     uploadImage.on('state_changed', 
     (snapshot) => {
-      // progrss function ....
-/*       const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-      this.setState({progress}); */
     }, 
     (error) => {
-         // error function ....
       console.log(error);
-    }, 
-  () => {
-      // complete function ....
-      storage.ref('Pilots/Pilot-x/').child(this.state.name).getDownloadURL().then(url => {
-          console.log(url);
-          this.setState({url},() => {
-            const newPortfolio = {
-              name: this.state.name,
-              id: this.state.id + 1,
-              url: this.state.url,
-              selectedFile: this.state.selectedFile
-            }
-            this.setState({
-              portfolios: this.state.portfolios.concat(newPortfolio),
-            });
+    },
+    () => {
+      storage.ref(`Pilots/Pilot-${this.state.pilotId}/`).child(this.state.name).getDownloadURL().then(url => {
+        this.setState({url},() => {
+
+          const newPortfolio = {
+            name: this.state.name,
+            id: this.state.id + 1,
+            url: this.state.url,
+            selectedFile: this.state.selectedFile
+          }
+
+          this.setState({
+            portfolios: this.state.portfolios.concat(newPortfolio),
           });
+
+          axios({
+            url: 'localhost:8000/media/crear',
+            method: 'POST',
+            data: {
+              pilotId: this.state.pilotId,
+              url: this.state.url,
+              type: "image",
+            }
+          })
+          .then(({ data }) => this.setState({ post: data }))
+          .catch((error) => this.setState({ error }));
+        });
       })
     });
   }
@@ -116,7 +136,7 @@ class Portfolios extends React.Component{
       <ComponentContainer>
         <h2>Portafolio</h2>
         <IconContainer>
-              <img src="https://image.flaticon.com/icons/svg/1096/1096090.svg" alt="Hola"></img>
+          <img src="https://image.flaticon.com/icons/svg/1096/1096090.svg"></img>
         </IconContainer>
         <p>Este espacio corresponde al material que te gustaria mostrar a
             los clientes. Relaciona todas tus mejores trabajos en fotos o
@@ -137,9 +157,8 @@ class Portfolios extends React.Component{
           <PortfoliosComponent portfolios = {this.state.portfolios}/>
         </PortfolioImageContainer>
       </ComponentContainer>
-
     )
-    }
+  }
 }
 
 export default Portfolios
