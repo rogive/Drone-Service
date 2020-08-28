8d64baea78028eeeaae4657a7eb741542d570836
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2020-08-27 22:32:55 -0500

    Adds validation to phone field

--- src/Components/FormHook.js
+++ src/Components/FormHook.js
@@ -7,20 +7,22 @@ import Departments from '../data/deparments.json'
 
 const FullContainer = styled.div `
   display: flex;
-  justify-content: space-between;
-  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const FormContainer = styled.form`
-  display: block;
-  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400%;
 `
 
 const FormFieldset = styled.fieldset`
   display:flex;
   width: 50%;
-  margin: 1rem auto;
   float: left;
+  margin: 1rem;
 `
 
 const FormLabel = styled.label`
@@ -40,13 +42,29 @@ const FormInput = styled.input`
   font-size: 1.2rem;
 `
 
-const FormHook = () => {
-  const [ currDepartment, setCurrDepartment ] = useState(0)
-  const [ currCities, setCurrCities ] = useState([])
+const FormButton = styled.button`
+    text-decoration: none;
+    padding: 1rem 1.6rem;
+    border-radius: 8px;
+    font-family: 'Montserrat';
+    font-size: 2rem;
+    color: white;
+    border: none;
+    background-image: linear-gradient(to left, rgba(10,10,200, 1), rgba(10,100,200, 1));
+    cursor: pointer;
 
+    &:hover{
+        background-image: linear-gradient(to left, rgba(10,10,100, 1), rgba(10,10,100, 1));
+    }
+`
+
+const FormHook = () => {
+
+  const [ currCities, setCurrCities ] = useState(Departments.filter(e => e.id === 0)[0].ciudades)
   const {register, errors, handleSubmit} = useForm()
   const history = useHistory()
   const emailRegexp = /((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))/;
+  const phoneRegexp = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\./0-9]*$/
 
   const onSubmit = data => {
     axios({
@@ -62,10 +80,9 @@ const FormHook = () => {
   }
 
   const mapDepartments = (departments) => {
-
     return departments.map( element => {
       return(
-        <option value={element.id}>
+        <option value={element.id} key={element.id}>
           {element.label}
         </option>
       )
@@ -73,10 +90,9 @@ const FormHook = () => {
   }
 
   const mapCities = (cities) => {
-
     return cities.map( element => {
       return(
-        <option value={element}>
+        <option value={element} key={element}>
           {element}
         </option>
       )
@@ -85,8 +101,8 @@ const FormHook = () => {
 
   return(
     <FullContainer>
-      <h1>Formulario</h1>
       <FormContainer onSubmit={handleSubmit(onSubmit)}>
+      <h1>Formulario</h1>
         <FormFieldset>
           <FormLabel htmlFor="name">Nombres: </FormLabel>
           <FormInput
@@ -144,7 +160,10 @@ const FormHook = () => {
             id="phone"
             name="phone"
             type="text"
-            ref={register()}
+            ref={register({
+              pattern: {value: phoneRegexp, message: 'Número de celular inválido'},
+              minLength: {value: 10, message: 'El número de celular debe tener mínimo 10 caracteres'}
+            })}
           />
           <span style={{color: "red"}}>
             {errors.phone?.message}
@@ -156,17 +175,11 @@ const FormHook = () => {
             id="department"
             name="department"
             type="text"
-            ref={register({ required: { value:true, message: 'El campo departamento es requerido' }})}
-            onChange={ event => { 
-              setCurrDepartment(event.target.value)
-              setCurrCities(Departments.filter(e => e.id === parseInt(event.target.value))[0].ciudades)
-            }}
+            ref={register({ required: true })}
+            onChange={ event => setCurrCities(Departments.filter(e => e.id === parseInt(event.target.value))[0].ciudades)}
           >
             {mapDepartments(Departments)}
           </select>
-          <span style={{color: "red"}}>
-            {errors.department?.message}
-          </span>
         </FormFieldset>
         <FormFieldset>
           <FormLabel htmlFor="city">Ciudad: </FormLabel>
@@ -174,24 +187,12 @@ const FormHook = () => {
             id="city"
             name="city"
             type="text"
-            ref={register({ required: { value:true, message: 'El campo ciudad es requerido' }})}
+            ref={register({ required: true })}
           >
             {mapCities(currCities)}
           </select>
-          <span style={{color: "red"}}>
-            {errors.city?.message}
-          </span>
         </FormFieldset>
-        <FormFieldset>
-          <select name="userType" ref={register({ required: true})}>
-            <option value="client">Cliente</option>
-            <option value="pilot">Piloto</option>
-          </select>
-          <span style={{color: "red"}}>
-            {errors.userType?.message}
-          </span>
-        </FormFieldset>
-        <button>Enviar</button>
+        <FormButton>Enviar</FormButton>
       </FormContainer>
     </FullContainer>
   )
