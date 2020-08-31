ef334ced4d8503d5dbd9b199c14420f1e5c7305c
Jonathan Palacio <jpalacio0612@gmail.com>
2020-08-31 10:09:51 -0500

    Change of post by get in Result.js

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,43 +1,42 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-
-function Results({info}){
-
+function Results({ info }) {
   const [pilotsDb, setPilotsDb] = useState([]);
-  
+
   useEffect(() => {
-    
-    setPilotsDb([])
-    
+    setPilotsDb([]);
+
     const filterPilots = async () => {
-      try{
-        const result = await axios.post('http://localhost:8000/pilotos/filtrar',{info})
-        setPilotsDb(result.data)
-      }catch(error){
-        alert(error)
+      try {
+        const result = await axios.get(
+          "http://localhost:8000/pilotos/filtrar",
+          { info }
+        );
+        setPilotsDb(result.data);
+      } catch (error) {
+        alert(error);
       }
-    }
-    filterPilots()
-  }, [info])
+    };
+    filterPilots();
+  }, [info]);
 
-
-  return(
+  return (
     <div>
       <h2>Resultados</h2>
-      { pilotsDb ?
-        pilotsDb.map((element)=>{
-          return <div key={element._id}>
-                    <img src={element.image} alt=""/>
-                    <h3>{element.name}</h3>
-                    <p>{element.description}</p>
-                 </div>
-              }
-            ) : null
-          }
+      {pilotsDb
+        ? pilotsDb.map((element) => {
+            return (
+              <div key={element._id}>
+                <img src={element.image} alt="" />
+                <h3>{element.name}</h3>
+                <p>{element.description}</p>
+              </div>
+            );
+          })
+        : null}
     </div>
-  )    
+  );
 }
 
-
-export default Results
+export default Results;
