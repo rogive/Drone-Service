2a730392dc698d43b9c62ee446abea9e826481b6
Jonathan Palacio <jpalacio0612@gmail.com>
2020-09-03 13:11:43 -0500

    added client profile page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './App.css';
-import DronePilot from './Pages/DronePilot'; 
-import Home from './Pages/Home';
-import Header from './Components/Header';
-import Explora from './Pages/Explora';
-import UserRegistry from './Pages/UserRegistry';
-import Error404 from './Pages/Error404';
-import Login from './Pages/Login'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import DronePilot from "./Pages/DronePilot";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+import Explora from "./Pages/Explora";
+import UserRegistry from "./Pages/UserRegistry";
+import Error404 from "./Pages/Error404";
+import Login from "./Pages/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ClientProfile from "./Pages/ClientProfile";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/explora" component={Explora} />
-          <Route exact path="/pilot-profile" component={DronePilot} />
-          <Route exact path="/user-registry" component={UserRegistry} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="*" component={Error404}/>
-        </Switch> 
-    </BrowserRouter>  
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/explora" component={Explora} />
+        <Route exact path="/client-profile" component={ClientProfile} />
+        <Route exact path="/pilot-profile" component={DronePilot} />
+        <Route exact path="/user-registry" component={UserRegistry} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="*" component={Error404} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-
 
 export default App;
---
+++ src/Pages/ClientProfile.js
@@ -0,0 +1,25 @@
+import React from "react";
+import GlobalContainer from "../utils/GlobalStyles";
+import "./ClientProfile.css";
+import UpdateClientProfile from "../Components/UpdateClientProfile";
+
+function ClientProfile() {
+  return (
+    <GlobalContainer>
+      <div className="title__clientProfile">
+        <h1>Hola Jonathan!</h1>
+        <button>Haz una solicitud</button>
+      </div>
+      <div className="content__clientProfile">
+        <div className="solicitudes__clientProfile">
+          <h2>Tus solicitudes</h2>
+        </div>
+        <div className="Updateprofile__clientProfile">
+          <UpdateClientProfile />
+        </div>
+      </div>
+    </GlobalContainer>
+  );
+}
+
+export default ClientProfile;
