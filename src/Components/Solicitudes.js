0135ff7864ea73f3913ea78f0af918d81565bbe8
ivan <ierodriguezgo@gmail.com>
2020-09-14 09:40:21 -0500

    solicitude.css created

--- src/Components/Solicitudes.js
+++ src/Components/Solicitudes.js
@@ -1,222 +1,20 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import styled from 'styled-components'
 import Categories from '../data/categories.json'
 import { storage } from '../firebase';
 import Departments from '../data/deparments.json'
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
+import "./Solicitudes.css";
 
-const FullContainer = styled.div `
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-image: url("https://images.pond5.com/drone-quadcopter-forest-background-footage-087770961_prevstill.jpeg");
-  background-color: #E5E5E5;
-  background-repeat: no-repeat;
-  background-size: Auto;
-`
-const FormContainer = styled.form`
-  display: flex;
-  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-  flex-direction: column;
-  align-items: center;
-  width: 50rem;
-  margin: 4rem Auto;
-  background-color: #E7ECF7;
-  opacity: 0.85;
-  .boxtitleform{
-    display: flex;
-    width: 100%;
-    height: 8rem;
-    align-items: center;
-    justify-content: center;
-    margin: 0rem 0rem 2rem 0rem;
-    background-color: rgb(0, 23, 105);
-    color: white;
-  }
-`
-
-const FormFieldset = styled.fieldset`
-  display:flex;
-  flex-direction: column;
-  width: 75%;
-  float: left;
-  border: none;
-  padding: 0.5rem 0rem 0.5rem 0rem;
-  .input-full-container {
-    display:flex;
-    flex-direction: row;
-    margin-bottom: 0.5rem;
-  }
-  .label-input-container{
-    width: 55%;
-  }
-  .input-container{
-    float: right;
-    width: 45%;
-  }
-  .error-input-container{
-    color: red;
-    float: right;
-    width: 40%;
-  }
-  progress{
-    width: 100%;
-  }
-  .input-title{
-    width: 89%;
-    height: 2rem;
-    float: right;
-    margin-left: 1rem;
-    background-color: white;
-    border: 1px solid gray;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-  }
-  .input{
-    width: 90%;
-    height: 2rem;
-    float: right;
-    margin-left: 1rem;
-    background-color: white;
-    border: 1px solid gray;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-  }
-
-  .inputtextarea{
-    background-color: white;
-    width: 100%;
-    height: 20vh;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-  }
-  
-  select{
-    height: 2rem;
-  }
-  .custom-file-upload {
-    border: 1px solid #ccc;
-    display: inline-block;
-    padding: 6px 12px;
-    cursor: pointer;    
-  }
-
-  .fullcontaineruploadimage{
-    height: 2rem;
-    display: flex;
-    flex-direction: row;
-    border-right: 1px solid gray;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-  }
-  .boxlabelnameimage {
-    width: 80%;
-    background-color: white;
-    border: 1px solid gray;
-  }
-  .boxbuttonsimage{
-    width: 20%;
-    display: flex;
-    flex-direction: row;
-    
-  }
-
-  .labelbuttonaddimage,
-  .labelbuttonuploadimage {
-    display: flex;
-    width: 5rem;
-    padding: 0.2rem 0rem 0.2rem 0rem;
-    font-size: 1rem;
-    font-weight: 500;
-    color: black;
-    justify-content: center;
-    text-align: center;
-    border: 1px solid gray;
-    background-color: #D9DFEE;
-  }
-
-  .labelnameimage {
-    display: flex;
-    height: 2rem;
-    justify-content: left;
-    align-items: center;
-    padding-left: 0.5rem;
-  }
-
-  .inputfile{
-    width: 0.1px;
-    height: 0.1px;
-    opacity: 0;
-    overflow: hidden;
-    position: absolute;
-    z-index: -1;
-  }
-  .buttonuploadimage{
-    width: 0.1px;
-    height: 0.1px;
-    opacity: 0;
-    overflow: hidden;
-    position: absolute;
-    z-index: -1;
-  }
-
-
-  .inputfile:focus, .labelbuttonaddimage:hover,
-  .buttonuploadimage:focus, .labelbuttonuploadimage:hover {
-    background-color: #D1D7E8;
-  }
-
-  
-`
-const FormLabel = styled.label`
-  margin: 1rem 0 1rem 0rem;
-  font-size: 1.6rem;
-`
-
-const FormButton = styled.button`
-    text-decoration: none;
-    margin: 2rem 0rem 2rem 0rem;
-    padding: 1rem 1.6rem;
-    margin-top: 3rem;
-    border-radius: 8px;
-    font-family: 'Montserrat';
-    font-size: 2rem;
-    color: white;
-    border: none;
-    background-image: linear-gradient(to left, rgba(10,10,200, 1), rgba(10,100,200, 1));
-    cursor: pointer;
-
-    &:hover{
-        background-image: linear-gradient(to left, rgba(10,10,100, 1), rgba(10,10,100, 1));
-    }
-`
-
-const ImageContainer = styled.div`
-  width: 17%;
-  border-radius: 1rem;
-  margin: 1rem;
-  img{
-    width: 5rem;
-    height: 5rem;
-    border-radius: 1rem;
-  }
-`
-
-const FullImageContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  width: 100%;
-`
 function ServiceImagesComponent({
   images
 }) {
   return images.map((image) => {
     return(
-        <ImageContainer>
-          <img src={image} alt=""></img>
-        </ImageContainer>
+        <div className="imagecontainer">
+          <img className="imagesolicitude" src={image} alt=""></img>
+        </div>
     );
   });
 }
@@ -308,15 +106,15 @@ const onSubmit = data => {
   }
 
   return(
-    <FullContainer>
-      <FormContainer onSubmit={handleSubmit(onSubmit)}>
+    <div className="fullcontainer">
+      <form className="formcontainer" onSubmit={handleSubmit(onSubmit)}>
         <div className="boxtitleform">
           <h1 className="titleform">Solicitud de Servicio</h1>
         </div>
-        <FormFieldset>
+        <fieldset className="formfieldset">
           <div className="input-full-container">
             <div className="label-input-container">
-              <FormLabel htmlFor="servicetitle">Titulo:</FormLabel>
+              <label className="formlabel" htmlFor="servicetitle">Titulo:</label>
             </div>
             <div className="input-container">
               <input
@@ -332,11 +130,11 @@ const onSubmit = data => {
               {errors.servicetitle?.message}
             </span>
           </div>
-        </FormFieldset>
-        <FormFieldset>
+        </fieldset>
+        <fieldset className="formfieldset">
           <div className="input-full-container">
             <div className="label-input-container">
-              <FormLabel htmlFor="servicetype">Servicio: </FormLabel>
+              <label className="formlabel" htmlFor="servicetype">Servicio: </label>
             </div>
             <div className="input-container">
               <select
@@ -350,11 +148,11 @@ const onSubmit = data => {
               </select>
             </div>
           </div>
-        </FormFieldset>
-        <FormFieldset>
+        </fieldset>
+        <fieldset className="formfieldset">
         <div className="input-full-container">
             <div className="label-input-container">
-              <FormLabel htmlFor="certifiedpilot">¿Piloto certificado?</FormLabel>
+              <label className="formlabel" htmlFor="certifiedpilot">¿Piloto certificado?</label>
             </div>
             <div className="input-container">
               <select
@@ -370,11 +168,11 @@ const onSubmit = data => {
               </select>
             </div>
           </div>
-        </FormFieldset>
-        <FormFieldset>
+        </fieldset>
+        <fieldset className="formfieldset">
           <div className="input-full-container">
             <div className="label-input-container">
-              <FormLabel htmlFor="ownequipment">¿Piloto con equipo?</FormLabel>
+              <label className="formlabel" htmlFor="ownequipment">¿Piloto con equipo?</label>
             </div>
             <div className="input-container">
               <select
@@ -392,13 +190,13 @@ const onSubmit = data => {
               </select>
             </div>
           </div>
-        </FormFieldset>
+        </fieldset>
         {
             ownequipment === "Si" ? (
-            <FormFieldset>
+            <fieldset className="formfieldset">
               <div className="input-full-container">
                 <div className="label-input-container">
-                  <FormLabel htmlFor="requireequipment">¿Alguno en especifico? </FormLabel>
+                  <label className="formlabel" htmlFor="requireequipment">¿Alguno en especifico? </label>
                 </div>
                 <div className="input-container">
                   <select
@@ -415,17 +213,17 @@ const onSubmit = data => {
                   </select>
                 </div>
               </div>
-            </FormFieldset>
+            </fieldset>
               ) :
               <span></span>
         }
         {
           requireequipment === "Si" ? (
-            <FormFieldset>
+            <fieldset className="formfieldset">
               <div>
                 <div className="input-full-container">
                   <div className="label-input-container">
-                    <FormLabel htmlFor="equipment">¿Cuál equipo?</FormLabel>
+                    <label className="formlabel" htmlFor="equipment">¿Cuál equipo?</label>
                   </div>
                   <div className="input-container">
                     <input
@@ -442,14 +240,14 @@ const onSubmit = data => {
                   </span>
                 </div>
               </div>
-            </FormFieldset>
+            </fieldset>
               ) :
               <span></span>
         }
-        <FormFieldset>
+        <fieldset className="formfieldset">
           <div className="input-full-container">
             <div className="label-input-container">
-              <FormLabel htmlFor="department">Departamento:</FormLabel>
+              <label className="formlabel" htmlFor="department">Departamento:</label>
             </div>
             <div className="input-container">
               <select
@@ -464,11 +262,11 @@ const onSubmit = data => {
               </select>
             </div>
           </div>
-        </FormFieldset>
-        <FormFieldset>
+        </fieldset>
+        <fieldset className="formfieldset">
           <div className="input-full-container">
             <div className="label-input-container">
-              <FormLabel htmlFor="city">Ciudad: </FormLabel>
+              <label className="formlabel" htmlFor="city">Ciudad: </label>
             </div>
             <div className="input-container">
               <select
@@ -482,12 +280,12 @@ const onSubmit = data => {
               </select>
             </div>
           </div>
-        </FormFieldset>
-        <FormFieldset>
+        </fieldset>
+        <fieldset className="formfieldset">
           <div>
             <div className="input-full-container">
               <div className="label-input-container">
-                <FormLabel htmlFor="dateservice">Fecha: </FormLabel>
+                <label className="formlabel" htmlFor="dateservice">Fecha: </label>
               </div>
               <div className="input-container">
                 <input type="date"
@@ -504,9 +302,9 @@ const onSubmit = data => {
             </div>
           </div>
 
-        </FormFieldset>
-        <FormFieldset>
-          <FormLabel> Imagenes: </FormLabel>
+        </fieldset>
+        <fieldset className="formfieldset">
+          <label> Imagenes: </label>
             <div className="fullcontaineruploadimage">
               <div className="boxlabelnameimage">
                 <label htmlFor="inputfile"
@@ -536,14 +334,14 @@ const onSubmit = data => {
                       >Cargar</label>
               </div>
           </div>
-        </FormFieldset>
-        <FormFieldset>
-          <FullImageContainer>
+        </fieldset>
+        <fieldset className="formfieldset">
+          <div class="fullimagecontainer">
             <ServiceImagesComponent images= {imagespreview}/>
-          </FullImageContainer>
-        </FormFieldset>
-        <FormFieldset>
-          <FormLabel htmlFor="description">Descripción:</FormLabel>
+          </div>
+        </fieldset>
+        <fieldset className="formfieldset">
+          <label className="formlabel" htmlFor="description">Descripción:</label>
           <textarea
                 id="description"
                 name="description"
@@ -553,10 +351,10 @@ const onSubmit = data => {
           <span style={{color: "red"}}>
             {errors.description?.message}
           </span>
-        </FormFieldset>
-        <FormButton>Enviar</FormButton>
-      </FormContainer>
-    </FullContainer>
+        </fieldset>
+        <button className="formbutton">Enviar</button>
+      </form>
+    </div>
   )
 }
 
