34a2975565bf6f2b310f0a96683fa50d833d6f5d
ivan <ierodriguezgo@gmail.com>
2020-09-19 02:11:28 -0500

    DroneBot v1.0.0

--- src/Components/ChatBot.js
+++ src/Components/ChatBot.js
@@ -62,13 +62,7 @@ class ActionProvider {
 
   
   updateChatbotState(message) {
- 
-  // NOTE: This function is set in the constructor, and is passed in      
-  // from the top level Chatbot component. The setState function here     
-  // actually manipulates the top level state of the Chatbot, so it's     
-  // important that we make sure that we preserve the previous state.
- 
-  this.setState(prevState => ({
+    this.setState(prevState => ({
     	...prevState, messages: [...prevState.messages, message]
     }))
   }
