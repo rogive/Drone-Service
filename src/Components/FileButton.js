3e852c6122330e5dd33ad20b63f42f1d20b69e81
rogive <66136788+rogive@users.noreply.github.com>
2020-09-27 18:30:30 -0500

    Others Component Updated (#24)
    fileform component created

--- src/Components/FileButton.js
+++ src/Components/FileButton.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import "./FileButton.css"
 
-function Button(props) {
+function Button({onChange, name, onSubmit, number, type}) {
   return (
     <div className="fullcontainerupload">
-      <div className={ props.number === 1 ? "boxlabelnameimage1" : "boxlabelnameimage2"}>
+      <div className={ number === 1 ? "boxlabelnameimage1" : "boxlabelnameimage2"}>
         <label htmlFor="inputfileadd"
-               className="labelnameimage">{ props.name || `Adjuntar ${props.type==="document"? "un documento" : "una imagen"}`}
+               className="labelnameimage">{ name || `Adjuntar ${type==="document"? "un documento" : "una imagen"}`}
         </label>
       </div>
-      <div className={ props.number === 1 ? "boxbuttonsimage1" : "boxbuttonsimage2"}>
+      <div className={ number === 1 ? "boxbuttonsimage1" : "boxbuttonsimage2"}>
         <input type="file"
             id="inputfileadd"
             name="inputfileadd"
-            onChange={props.onChange}
+            onChange={onChange}
             className="inputfileadd"
             style={{display: 'none'}}
-            accept={props.type==="document"? "pdf" : ".jpg, .jpeg, .png"}
+            accept={type==="document"? "pdf" : ".jpg, .jpeg, .png"}
             />
         <label 
             htmlFor="inputfileadd" 
-            className={ props.number === 1 ? "labelbuttonadddocument1" : "labelbuttonadddocument2"}
+            className={ number === 1 ? "labelbuttonadddocument1" : "labelbuttonadddocument2"}
             >Examinar</label>
-        {props.number === 2 && (
+        {number === 2 && (
           <>
             <button type="button"
                     id="buttonuploadimage"
                     name="buttonuploadimage"
                     className="buttonuploadimage" 
-                    onClick={props.onSubmit}
+                    onClick={onSubmit}
                     >subir</button>
             <label htmlFor="buttonuploadimage" 
                     className={"labelbuttonuploaddocument"}
@@ -41,5 +41,3 @@ function Button(props) {
 }
 
 export default Button
-
-  
--- src/Components/Others.js
+++ src/Components/Others.js
@@ -1,70 +1,41 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import FileButton from './FileButton'
+import React, {useState, useEffect} from 'react';
+import { storage } from '../firebase';
+import FileForm from './FileForm';
+import axios from 'axios';
+import "./Others.css"
 
-const DocumentsContainer = styled.div`
-  width: 100%;
-  background-color: #66b2ff;
-  color: black;
-  border-radius: 2rem;
-  padding: 1rem;
-  margin-bottom: 2rem;
-  p{
-    padding: 1rem;
-    text-align: left;
-  }
-`
-
-const IconContainer = styled.div`
-  width: 100%;
-  text-align:center;
-  padding-top: 3rem;
-  img{
-    width: 15rem;
-    height: 15rem;
-    border-radius: 3rem;
-    filter: grayscale(100%);
-  }
-`
-
-const AttachContainer = styled.div`
-  padding-top:2rem;
-  padding-bottom:2rem;
-`
-
-const ComponentContainer = styled.div`
-  p{
-    padding-top: 2rem;
-    text-align: justify;
-    font-size: 1.2rem;
-  }
-  h2{
-    font-size: 2rem;
-  }
-`
-
-
-function OthersComponent({
-  others
-  }) {
-    return others.map((other) => {
-      return(
-        <DocumentsContainer>
-          <p>{other.name}</p>
-        </DocumentsContainer>
-  
-      );
-  });
-  
+function OthersComponent({ others }) {
+  return others.map((other) => {
+    return(
+      <div className="documentscontainerothers">
+        <p className="elementothers">{other.title}</p>
+        <a href={other.url} target="_blank" rel="noopener noreferrer" className="urlothers">
+          <img src="https://firebasestorage.googleapis.com/v0/b/droneservice-cc42f.appspot.com/o/src%2Ficons%2Fdocument-icon.png?alt=media&token=57d39b43-a362-40ce-9652-08ef9af6388f" 
+                alt="pdfIcon"
+                className="iconothers"
+          />
+        </a>
+      </div>
+    )
+  })
 }
 
 function Others() {
-  const [certificates, setCertificates] = useState([])
+  const [documents, setDocuments] = useState([])
   const [name, setName] = useState('')
-  const [pilotId, setPilotid] = useState(sessionStorage.getItem("userId"))
-  const [urlDocument, setUrlDocument] = useState('')
   const [selectedFile, setSelectedFile] = useState(null)
   const [error, setError] = useState(null)
+  const [showadd, setShowAdd] = useState(false)
+  const pilotId = sessionStorage.getItem("userId")
+
+  useEffect( () => {
+    axios({
+      url: `http://localhost:8000/others/listar/piloto/${pilotId}`,
+      method: 'GET',
+    })
+      .then(({ data }) => setDocuments( data ) )
+      .catch((error) => setError({ error }))
+  }, [])
 
   function handleChange(event) {
     if(!event.target.files[0]) return
@@ -72,24 +43,59 @@ function Others() {
     setName(event.target.files[0].name)
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function onSubmit( data ) {
+    const uploadDocument = storage.ref(`Pilots/Pilot-${pilotId}/Others/` + name).put(selectedFile);
+    uploadDocument.on('state_changed', 
+      (snap) => {}, 
+      (error) => {alert(error)},
+      () => {
+        storage.ref(`Pilots/Pilot-${pilotId}/Others/`).child(name).getDownloadURL().then(url => {
+          axios({
+            url: 'http://localhost:8000/others/crear',
+            method: 'POST',
+            data: { ...data,
+              pilotId,
+              name,
+              url,
+              type: "document"
+            }
+          }).then(({ data }) => {
+            setDocuments( documents.concat(data) )
+            setShowAdd(!showadd)
+          }
+          )
+          .catch((error) => setError(error));
+        });
+      }
+    )
   }
 
   return(
-    <ComponentContainer>
-      <h2>Otros</h2>
-      <IconContainer>
-        <img src="https://cdn3.iconfinder.com/data/icons/election-world-1/64/senate-congress-government-senator-political-512.png" alt="Hola"></img>
-      </IconContainer>
-      <p>Este espacio corresponde a otros tipos de certificados generado en
-          la asistencia de eventos, congresos, seminarios o conferencias.
+    <>
+      <h2 className="titleothers">Otros</h2>
+      <div className="iconcontainerothers">
+        <img src="https://cdn3.iconfinder.com/data/icons/election-world-1/64/senate-congress-government-senator-political-512.png" 
+              alt="Hola"
+              className="imagecontentothers"
+              ></img>
+      </div>
+      <p className="description-other">
+        Aca podras incluir todos los documentos, seminarios, congresos u otros eventos que te han permitido conocer, 
+        aprender o obtener experiencia en algún area especifica relacionada a operaciones con Drones. Recuerda que 
+        solo se muestra el título del documento.
       </p>
-      <AttachContainer>
-      <FileButton onChange={handleChange} onSubmit={handleSubmit} name={name} number={1} type="document"/>
-      </AttachContainer>
-      <OthersComponent others = {certificates}/>
-    </ComponentContainer>
+      <br/>
+      <button className="add-document"
+              id="add-document"
+              onClick={()=>setShowAdd(!showadd)}
+              >+ Añadir documento</button>
+      {showadd && (
+        <FileForm onChange={handleChange} onSubmit={onSubmit} name={name} type="others"/>
+      )}
+      <div className="documentscomponentcontainer">
+        <OthersComponent others = {documents}/>
+      </div>
+    </>
   )
 }
 
