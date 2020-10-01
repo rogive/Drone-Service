d871c636a72983d44f9a759585d1900f0e11e568
Jonathan Palacio <jpalacio0612@gmail.com>
2020-09-30 22:24:51 -0500

    Add for view to pilotCard

--- src/Components/PilotCard.js
+++ src/Components/PilotCard.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./PilotCard.css";
 
 function PilotCard({ pilot }) {
+  const [viewShow, setViewShow] = useState("portfolio");
   console.log(pilot);
   return (
     <div class="pilotcard-container">
-      <div className="pilotcard-photos">
-        {pilot &&
-          pilot.media.map((image, index) => {
-            if (index < 3) {
-              return <img className="pilotcard-photo" src={image.url} alt="" />;
-            }
-          })}
+      {viewShow == "portfolio" && (
+        <div className="pilotcard-photos">
+          {pilot &&
+            pilot.media.map((image, index) => (
+              <img className="pilotcard-photo" src={image.url} alt="" />
+            ))}
+        </div>
+      )}
+      {viewShow == "equipos" && <div>Equipos</div>}
+      {viewShow == "certificaciones" && <div>Certificaciones</div>}
+      {viewShow == "servicios" && <div>Servicios</div>}
+      <div className="pilotcard-selector">
+        <div
+          onClick={() => setViewShow("portfolio")}
+          className={
+            viewShow == "portfolio" ? "tag-selector-selected" : "tag-selector"
+          }
+        >
+          <h2>Portafolio</h2>
+        </div>
+        <div
+          onClick={() => setViewShow("equipos")}
+          className={
+            viewShow == "equipos" ? "tag-selector-selected" : "tag-selector"
+          }
+        >
+          <h2>Equipos</h2>
+        </div>
+        <div
+          onClick={() => setViewShow("certificaciones")}
+          className={
+            viewShow == "certificaciones"
+              ? "tag-selector-selected"
+              : "tag-selector"
+          }
+        >
+          <h2>Certificaciones</h2>
+        </div>
+        <div
+          onClick={() => setViewShow("servicios")}
+          className={
+            viewShow == "servicios" ? "tag-selector-selected" : "tag-selector"
+          }
+        >
+          <h2>Servicios</h2>
+        </div>
       </div>
       <div className="pilotcard-description">
         <h1>{pilot.name}</h1>
